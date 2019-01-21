48cc8a8c584489115b807c369cfd1210bdf871e7
XxKarlozxX <carlosh2695@hotmail.com>
2019-01-21 08:50:11 -0600

    avance login

--- includes/lateral.php
+++ includes/lateral.php
@@ -1,48 +1,59 @@
 <?php require_once 'includes/helpers.php'; ?>
-    <aside id="sidebar">
-                <div id="login" class="bloque">
-                    <h3>Identificate</h3>
-                    <form action="login.php" method="POST">
-                        <label for="email" >Email</label>
-                        <input type="email" name="email">
+<aside id="sidebar">
+    <?php if( isset( $_SESSION['usuario'] ) ) : ?>
+        <div class="bloque">
+            <h3>Bienvenido, <?= $_SESSION['usuario']['nombre'] . ' ' . $_SESSION['usuario']['apellidos'] ?></h3>   
+            <a href="cerrar.php" class="boton">Cerrar sesión</a>
+        </div>
+    <?php endif; ?>
+    <div id="login" class="bloque">
+        <h3>Identificate</h3>
+        <?php if( isset( $_SESSION['error_login'] ) ) : ?>
+            <div class="alerta alerta-error">
+                <?= $_SESSION['error_login'] ?>   
+            </div>
+        <?php endif; ?>
+        <form action="login.php" method="POST">
+            <label for="email" >Email</label>
+            <input type="email" name="email">
 
-                        <label for="password" >Contraseña</label>
-                        <input type="password" name="password">
+            <label for="password" >Contraseña</label>
+            <input type="password" name="password">
 
-                        <input type="submit" value="Enviar">
-                    </form>
-                </div>
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
 
-                <div id="register" class="bloque">
-                    <h3>Registrate</h3>
-                    <?php if (isset( $_SESSION['completado'] ) ) : ?>
-                        <div class="alerta alerta-exito">
-                            <?= $_SESSION['completado'] ?>
-                        </div>
-                    <?php elseif ( isset( $_SESSION['errores']['generales'] ) ) :  ?>
-                        <div class="alerta alerta-error">
-                            <?= $_SESSION['errores']['generales'] ?>
-                        </div>
-                    <?php endif ?>
-                    <form action="registro.php" method="POST">
-                        <label for="nombre" >Nombre</label>
-                        <input type="text" name="nombre">
-                        <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'nombre' ) : ''; ?>
+    <div id="register" class="bloque">
+        <h3>Registrate</h3>
+        <?php if (isset( $_SESSION['completado'] ) ) : ?>
+        <div class="alerta alerta-exito">
+            <?= $_SESSION['completado'] ?>
+        </div>
+        <?php elseif ( isset( $_SESSION['errores']['generales'] ) ) :  ?>
+        <div class="alerta alerta-error">
+            <?= $_SESSION['errores']['generales'] ?>
+        </div>
+        <?php endif ?>
+        <form action="registro.php" method="POST">
+            <label for="nombre" >Nombre</label>
+            <input type="text" name="nombre">
+            <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'nombre' ) : ''; ?>
 
-                        <label for="apellidos" >Apellidos</label>
-                        <input type="text" name="apellidos">
-                        <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'apellidos' ) : ''; ?>
+            <label for="apellidos" >Apellidos</label>
+            <input type="text" name="apellidos">
+            <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'apellidos' ) : ''; ?>
 
-                        <label for="email" >Email</label>
-                        <input type="email" name="email">
-                        <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'email' ) : ''; ?>
+            <label for="email" >Email</label>
+            <input type="email" name="email">
+            <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'email' ) : ''; ?>
 
-                        <label for="password" >Contraseña</label>
-                        <input type="password" name="password">
-                        <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'password' ) : ''; ?>
+            <label for="password" >Contraseña</label>
+            <input type="password" name="password">
+            <?php echo isset($_SESSION['errores'] ) ? mostrar_error( $_SESSION['errores'], 'password' ) : ''; ?>
 
-                        <input type="submit" name="submit" value="Registrar">
-                    </form>
-                    <?php borrarErrores(); ?>
-                </div>
-            </aside>
+            <input type="submit" name="submit" value="Registrar">
+        </form>
+        <?php borrarErrores(); ?>
+    </div>
+</aside>
---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+// iniciar sesión y la conexion a la BD
+require_once 'includes/conexion.php';
+
+// Recoger los datos del formulario
+if ( $_POST ) {
+    $email = trim( $_POST['email'] );
+    $password = $_POST['password'];
+
+    // cosulta para comprobar las credenciasles del usuario
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $login = mysqli_query( $db, $sql );
+
+    if ( $login && mysqli_num_rows( $login ) == 1 ) {
+        $usuario = mysqli_fetch_assoc( $login );
+
+        $verify = password_verify( $password, $usuario['password'] );
+
+        if ( $verify ) {
+            // Guarda una sesion con los datos obtenidos
+            $_SESSION['usuario'] = $usuario;
+
+            if ( isset( $_SESSION['error_login'] ) ) {
+                session_unset( $_SESSION['error_login'] );
+            }
+
+        } else {
+            // Si algo falla enviar sesion de que algo ha salido mal
+            $_SESSION['error_login'] = "Login Incorrecto!!";
+        }
+    } else {
+        $_SESSION['error_login'] = "Login Incorrecto!!";
+    }
+
+  
+}
+
+// Rediriji al index
+
+header('Location: index.php');
